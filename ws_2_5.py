7a70e021195c1a1b0c29814ba397743bf59622a7
yuna <yunao1@naver.com>
2025-07-22 14:08:00 +0900

    final_commit

--- ws_2_5.py
+++ ws_2_5.py
@@ -1,3 +1,5 @@
+import copy
+
 catalog = [
     ['시간의 틈', '반짝임의 어둠', '망각의 경계'],
     ['연기의 수수께끼', '장면의 고백', '드라마의 그림자'],
@@ -12,9 +14,16 @@ backup_catalog = None
 '성공을 향한 한 걸음', '내 삶의 변화', '목표 달성의 비밀' 가 되도록 변경하시오.
 '''
 
+backup_catalog = copy.deepcopy(catalog)
+
 print('catalog와 backup_catalog를 비교한 결과')
 # 식별 연산자로 catalog와 backup_catalog를 비교한 결과를 출력하시오.
 print()
+
+catalog[3][0] = '성공을 향한 한 걸음'
+catalog[3][1] = '내 삶의 변화'
+catalog[3][2] = '목표 달성의 비밀'
+
 
 print('backup_catalog : ')
 print(backup_catalog)
