8b7d40cb8970140fc7e8c986060e931bf1911a6c
yuna <yunao1@naver.com>
2025-08-11 23:25:05 +0900

    com

--- 0811_test/algo1_01_kimyuna.py
+++ 0811_test/algo1_01_kimyuna.py
@@ -1,46 +1,52 @@
-# import sys
-#
-# sys.stdin = open('algo1_sample_in.txt')
+import sys
+
+sys.stdin = open('algo1_sample_in.txt')
 #테스트케이스 입력 받기
 T = int(input())
 # print(T)
 for tc in range(1, T+1):
     # M : 탐색할 수 있는 범위 , N : 우주 공간 , K :  별의 갯수
     N, M, K = input().split()
-    # N = int(N)
-    # M = int(N)
-    # K = int(N)
+    N = int(N)
+    M = int(M)
+    K = int(K)
     # print(N)
     # print(M)
     # print(K)
-    arr = [list(input()) for _ in range(N)] # N*N의 배열 입력 받기
+
+    # N*N의 배열 입력 받기
+    arr = [list(input().strip()) for _ in range(N)]
     # print(arr)
 
-    #좌표의 이동
+    #좌표의 이동 (사용하지 않지만 기존 코드 보존)
     dr = [-1,-1,0,0]
     dc = [0,0,-1,-1]
 
-
     # 탐색할 수 있는 영역
     # 영역 안에 K 개의 별을 포함해야 함
-    for serch in round(N):
-        #별의 갯수를 세기 위한 변수 초기화
-        number_of_star = 0
-        for see in round(serch):
-            #좌표의 초기 위치 설정
-            serch_area = arr[0][0]
-            # serch가 움직일 때 마다 see가 별 갯수 확인
-            if '*' in serch_area:
-                number_of_star += 1
-                # 별의 갯수가 K와 같아질 때 정답 출력
-                if number_of_star == K:
-                    print(f'#{tc} {dr} {dc}')
-                #K 개의 별의 갯수를 찾을 수 없다면 -1 -1 출력
-                elif number_of_star == 0:
-                    print(f'#{tc} -1 -1')
+    found = False
+    for serch in range(N - M + 1):         # 행 방향(좌상단 r)
+        #별의 갯수를 세기 위한 변수는 각 열 시작 시 초기화
+        for see in range(N - M + 1):       # 열 방향(좌상단 c)
+            number_of_star = 0
+            #좌표의 초기 위치 설정 = 현재 MxM 윈도우 좌상단
+            r0, c0 = serch, see
 
+            # serch가 움직일 때 마다 see가 별 갯수 확인 (MxM 내부 스캔)
+            for r in range(r0, r0 + M):
+                for c in range(c0, c0 + M):
+                    if arr[r][c] == '*':
+                        number_of_star += 1
 
+            # 별의 갯수가 K와 같아질 때 정답 출력
+            if number_of_star == K:
+                print(f'#{tc} {r0} {c0}')
+                found = True
+                break
 
+        if found:
+            break
 
-
-
+    # K 개의 별의 갯수를 찾을 수 없다면 -1 -1 출력
+    if not found:
+        print(f'#{tc} -1 -1')
--- 0811_test/algo2_01_kimyuna.py
+++ 0811_test/algo2_01_kimyuna.py
@@ -1,40 +1,40 @@
-# import sys
-#
-# sys.stdin = open('algo2_sample_in.txt')
-#테스트케이스 입력 받기
-T = int(input())
-# print(T)
-for tc in range(1, T+1):
-    # M , N : 2차원 배열
-    N, M = input().split()
-    N = int(N)
-    M = int(N)
-    # print(N)
-    # print(M)
-    arr = [list(input().split()) for _ in range(N)] #배열의 값
-    # print(arr)
+import sys
+sys.stdin = open('algo2_sample_in.txt')
 
-    #N과 M의 배열에 대해서 안전 구역을 돌면서 안전 구역의 수 확인
+T = int(input())
+for tc in range(1, T+1):
+    # N: 행의 개수, M: 열의 개수
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    # 안전 구역 개수 초기화
+    number_of_safe_area = 0
+
+    # ↖ (-1,-1), ↗ (-1,+1), ↙ (+1,-1), ↘ (+1,+1)
+    dr = [-1, -1, 1, 1]
+    dc = [-1,  1, -1, 1]
+
+    # 전체 N×M 배열을 순회
     for i in range(N):
         for j in range(M):
-            #현재 위치 표시
-            present_area = arr[i][j]
-            #안전 구역의 수 계산을 위한 변수
-            number_of_safe_area = 0
-            # 만약 현재 위치의 값이 인접 위치의 값보다 크다면
-            if present_area > present_area[i+1][j+1]:
-                if present_area > present_area[i+1][j-1]:
-                    if present_area > present_area[i-1][j+1]:
-                        if present_area > present_area[i-1][j-1]:
-                            #안전 구역 갯수 더하기
-                            number_of_safe_area += 1
-            #정답 출력
-            print(f'#{tc} {number_of_safe_area}')
-            if i < 0 or i > M:
-                break
-            elif i < 0 or i > M:
-                break
+            # 현재 위치가 안전한지 여부
+            safe = True
 
+            # 4개의 대각선 방향을 검사
+            for k in range(4):
+                ni = i + dr[k]  # 이웃 셀의 행 좌표
+                nj = j + dc[k]  # 이웃 셀의 열 좌표
 
+                # 이웃 좌표가 배열 범위 안에 있을 때만 비교
+                if 0 <= ni < N and 0 <= nj < M:
+                    # 현재 값보다 크거나 같은 이웃이 있으면 안전하지 않음
+                    if arr[ni][nj] > arr[i][j]:
+                        safe = False
+                        break
 
+            # 모든 대각선 이웃보다 크면 안전 구역으로 카운트
+            if safe:
+                number_of_safe_area += 1
 
+    # 결과 출력
+    print(f'#{tc} {number_of_safe_area}')
