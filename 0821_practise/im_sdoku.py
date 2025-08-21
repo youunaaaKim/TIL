94e31b58f6094c9a89db07e597c7e04dd7d252ba
yuna <yunao1@naver.com>
2025-08-21 23:31:23 +0900

    com

--- 0821_practise/im_sdoku.py
+++ 0821_practise/im_sdoku.py
@@ -3,31 +3,37 @@ sys.stdin = open('sdoku.txt')
 
 T = int(input())
 for tc in range(1, T+1):
-    arr = [list(map(int, input().split())) for _ in range(1, 9)]
+    arr = [list(map(int, input().split())) for _ in range(9)]
     # print(arr)
 
+    valid = 1  # 유효하면 1, 아니면 0
+
     # 열과 행을 돌아가며 숫자 겹치는거 있는지 찾기
+    # 행 검사
     for r in arr:
-        c = [0] * 12  # 숫자 개수 저장용 리스트
+        c = [0] * 10
         for num in r:
             c[num] += 1
             if c[num] >= 2:
-                print(0)
+                valid = 0
 
+    # 열 검사
     for col in zip(*arr):
-        c = [0] * 12
+        c = [0] * 10
         for num in col:
             c[num] += 1
             if c[num] >= 2:
-                print(0)
+                valid = 0
 
+    # 3x3 검사
+    for i in range(0, 9, 3): # i: 0, 3, 6 (행의 시작 인덱스)
+        for j in range(0, 9, 3): # j: 0, 3, 6
+            c = [0] * 10
+            for x in range(3): # 3x3 구역의 행 탐색
+                for y in range(3): # 3x3 구역의 열 탐색
+                    num = arr[i + x][j + y] # 3x3 구역의 각 숫자
+                    c[num] += 1
+                    if c[num] >= 2:
+                        valid = 0
 
-
-
-    # 3*3 도 보기
-    # for r in range():
-
-
-
-
-
+    print(valid)
--- D1/23795_space_monster.py
+++ D1/23795_space_monster.py
@@ -9,40 +9,9 @@ T = int(input()) # 테스트케이스
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    #print(arr)
+    print(arr)
 
     for r in range(N):
-<<<<<<< HEAD
-        for c in range(N):
-            if arr[r][c] != 2:
-                continue
-
-            for i in range(4):  # 각 방향별 검사
-                nr, nc = r, c  # 현재 위치 초기화
-
-                # 각 칸은 0 (빈칸) 1(벽) 괴물(2) 중의 하나이다.
-                # 범위 체크에서 범위 안에 있으면 while 반복, 한 방향으로 계속 전진하며 검사
-                while 0 <= nr < N and 0 <= nc < N:
-                    # 다음 위치 계산
-                    # (이 부분 틀림)
-                    # nr = r + dr[i]  # next_row  매번 기준 좌표 r에서 출발해서 한 칸 이동.
-                    # nc = c + dc[i]  # next_column 반복할 때마다 처음 좌표에서 1칸만 보는 것.
-                    nr += dr[i] # 이전에 이동한 좌표 nr에서 다시 다음 좌표로 계속 진행.
-                    nc += dc[i]
-
-                    # 이동 후 범위를 벗어나지 않는다면
-                    if 0 <= nr < N and 0 <= nc < N:
-                        # 벽을 만나면 해당 방향 종료(시야 차단)
-                        if arr[nr][nc] == 1:
-                            break
-                        # 0이면 1로 변경 (괴물 직선 가시 구역)
-                        if arr[nr][nc] == 0:
-                            arr[nr][nc] = 1
-                    else:
-                        break
-
-    #print(arr)
-=======
         for c in range(N): # 처음에 베열을 돌면서
             if arr[r][c] == 2:  # 괴물 발견함
                 for i in range(4):  # 괴물을 만났을 때 4방향으로 퍼뜨리기
@@ -58,10 +27,9 @@ for tc in range(1, T+1):
                         nc += dc[i]
 
     print(arr)
->>>>>>> f9ab00f1db32f359bde93e3036e62a105fa3e5ba
         # 살아남은 0의 개수 세기
     safe_count = sum(row.count(0) for row in arr)
-    print(f'#{tc} {safe_count}')
+    print(safe_count)
 
 
 
