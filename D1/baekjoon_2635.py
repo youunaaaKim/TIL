e376ba96ca78ced6e5db803b93b96ac28379e775
Junghyun NA <junghyun.na321@gmail.com>
2025-09-08 17:56:43 +0900

    com

--- D1/baekjoon_2635.py
+++ D1/baekjoon_2635.py
@@ -1,36 +1,17 @@
-# 8
-# 100 62 38 24 14 10 4 6
+n = int(input())
+max_len = 0
+final_seq = []
 
-
-
-input = 100
-answer = []
-su = 0
-for i in range(input-1, 0, -1):
-    temp = [input, i]
+for i in range(n, 0, -1):
+    seq = [n, i]
     while True:
-        next_num = answer[i] - answer[i-1]
+        next_num = seq[-2] - seq[-1]
         if next_num < 0:
             break
-        temp.append(next_num)
+        seq.append(next_num)
+    if len(seq) > max_len:
+        max_len = len(seq)
+        final_seq = seq
 
-        if len(temp) > max_len:
-            max_len = len(temp)
-            answer = temp
-
-    print(max_len)
-    print(*answer)
-
-
-
-
-    third = input - next_number
-    fourth = next_number - third
-
-
-
-
-print(su)
-print(answer)
-
-
+print(max_len)
+print(*final_seq)
